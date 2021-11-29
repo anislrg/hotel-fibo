c47f32a67b96267c14325d9b2a2f4fc5915dd2b7
Nassim <nakynae7@icloud.com>
2021-11-29 12:12:18 +0100

    Header Changement Couleur

--- codefibo/app.js
+++ codefibo/app.js
@@ -3,9 +3,9 @@ const header = document.querySelector(".header");
 window.onscroll = function(){
     var top = window.scrollY;
     console.log(top);
-    if(top >= 100){
-        header.classList.add(".active")
-    }else{
-        header.classList.remove(".active")
-    }
+    if (top >= 100){
+            header.classList.add('active')
+        }else{
+            header.classList.remove('active');
+        }
 }
